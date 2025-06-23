f0c32a6b13ac4bb126b077072f0c0f680613a057
Saransh Basu <saranshbasu@gmail.com>
2025-06-23 13:43:21 +0545

    generated new image using imgflip api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import { useState } from 'react'
+import { use } from 'react'
+import { useState,useEffect } from 'react'
 const Main = () => {
     const [meme,setMeme]=useState({
         topText:"Something different",
         bottomText:"Walk into Mordor",
         image:"http://i.imgflip.com/1bij.jpg",
     })
+    const [allMemes,setAllMemes]=useState([])
 
+    useEffect(() => {
+       fetch("https://api.imgflip.com/get_memes")
+       .then(res=>res.json())
+       .then(data=>setAllMemes(data.data.memes))
+
+    }, [])
+    
+    function getMemeImage(){
+        const randomNumber=Math.floor(Math.random() * allMemes.length)
+        const newMemeUrl=allMemes[randomNumber].url
+        setMeme(prevMeme=>({
+            ...prevMeme,
+            image:newMemeUrl
+        }))
+    }
     function handleChange(event){
         const {value,name}=event.currentTarget 
         //  or const value = event.currentTarget.value;
@@ -38,7 +55,7 @@ const Main = () => {
                          value={meme.bottomText}
                     />
                 </label>
-                <button>Get a new meme image 🖼</button>
+                <button onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src={meme.image} />
